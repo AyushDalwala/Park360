2020da2e0c2112f41af74d4ad007ebf1bc10d78c
ayushdalwala <intern7.park360@gmail.com>
2026-01-23 12:17:39 +0530

    api task updated

--- Week_2/JS_Task/scripting.js
+++ Week_2/JS_Task/scripting.js
@@ -114,5 +114,16 @@ function renderTable() {
     const users = JSON.parse(localStorage.getItem("users")) || [];
     const tbody = document.querySelector("#userTable tbody");
     tbody.innerHTML = "";
+
+    users.forEach(user => {
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.country}</td>
+            <td>${user.state}</td>
+            <td>${user.city}</td>
+        `;
+    });
 }
-    
